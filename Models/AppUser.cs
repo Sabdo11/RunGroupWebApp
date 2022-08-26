3fddc5f29132a840b4b5eb3a685ba02aac542d15
selamawit abdo <selamawitabdo@selamawits-MacBook-Pro.local>
2022-08-26 09:40:04 -0600

    inital commit

---
+++ Models/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public String Id { get; set; }
+
+        public int? Pace { get; set; }
+
+        public int? Mileage { get; set; }
+
+        public Address? Address { get; set; }
+
+
+        public ICollection<Club> Clubs { get; set; }
+
+        public ICollection<Race> Races { get; set; }
+
+    }
+}
+
